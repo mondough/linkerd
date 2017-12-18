5bd86ed2541af92c30e7855a4bd06bf3576dff43
Oliver Beattie <oliver@obeattie.com>
2017-12-18 11:58:06 +0000

    monzo-1.3.4-monzo.0

--- project/Base.scala
+++ project/Base.scala
@@ -32,7 +32,7 @@ object Base {
 class Base extends Build {
   import Base._
 
-  val headVersion = "1.3.4"
+  val headVersion = "1.3.4-monzo.0"
 
   object Git {
     def git(arg: String, args: String*) = Process("git" +: arg +: args)
