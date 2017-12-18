31dd6bafa872da9604ca09aff31f9d660ec630af
Oliver Beattie <oliver@obeattie.com>
2017-12-18 13:53:51 +0000

    Fix issue with stream termination
    I’ll deal with this more properly later

--- config/src/main/scala/io/buoyant/config/JsonStreamParser.scala
+++ config/src/main/scala/io/buoyant/config/JsonStreamParser.scala
@@ -7,9 +7,10 @@ import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
 import com.twitter.concurrent.AsyncStream
 import com.twitter.io.{Buf, Reader}
 import com.twitter.logging.Logger
-import com.twitter.util.Try
+import com.twitter.util.{Return, Throw, Try}
 
 import scala.collection.mutable
+import scala.util.control.NonFatal
 
 class JsonStreamParser(mapper: ObjectMapper with ScalaObjectMapper) {
 
@@ -44,6 +45,8 @@ class JsonStreamParser(mapper: ObjectMapper with ScalaObjectMapper) {
   def readChunked[T: TypeReference](chunk: Buf): (Option[T], Buf) = {
     val Buf.ByteArray.Owned(bytes, begin, end) = Buf.ByteArray.coerce(chunk)
     val parser = mapper.getFactory.createParser(bytes, begin, end - begin)
+    val Buf.Utf8(s) = chunk
+    log.info("trying json read on: %s", s)
 
     val (obj, offsetAfter) = try {
       val v = Option(parser.readValueAs[T](implicitly[TypeReference[T]]))
@@ -55,17 +58,37 @@ class JsonStreamParser(mapper: ObjectMapper with ScalaObjectMapper) {
       parser.close()
     }
 
-    if (obj.isDefined)
-      log.trace("json read object: %s", obj)
-
+    log.info("json read object: %s", obj)
     val leftover = chunk.slice(offsetAfter, chunk.length)
     (obj, leftover)
   }
 
+  private def fromReaderJson(r: Reader, chunkSize: Int = Int.MaxValue): AsyncStream[Option[Buf]] = {
+    log.trace("json reading chunk of %d bytes", chunkSize)
+    val read = r.read(chunkSize).respond {
+      case Return(Some(Buf.Utf8(chunk))) =>
+        log.trace("json read chunk: %s", chunk)
+      case Return(None) | Throw(_: Reader.ReaderDiscarded) =>
+        log.trace("json read eoc")
+      case Throw(e) =>
+        log.warning(e, "json read error")
+    }.handle {
+      case NonFatal(e) => None
+    }
+
+    AsyncStream.fromFuture(read).flatMap {
+      //Fake None element to get around scanLeft being one behind bug
+      //Tracked in https://github.com/twitter/util/issues/195
+      //Can be removed once 195 is fixed
+      case Some(buf) => Some(buf) +:: None +:: fromReaderJson(r, chunkSize)
+      case None => AsyncStream.empty[Option[Buf]]
+    }
+  }
+
   def readStream[T: TypeReference](reader: Reader, bufsize: Int = 8 * 1024): AsyncStream[T] = {
-    AsyncStream.fromReader(reader, bufsize)
+    fromReaderJson(reader, bufsize)
       .scanLeft[(Seq[T], Buf)]((Nil, Buf.Empty)) {
-        case ((_, lastLeftover), chunk) =>
+        case ((_, lastLeftover), Some(chunk)) =>
           var b = lastLeftover.concat(chunk)
           val objs = mutable.Buffer.empty[T]
           var reading = true
@@ -78,6 +101,8 @@ class JsonStreamParser(mapper: ObjectMapper with ScalaObjectMapper) {
             reading = o.isDefined
           }
           (objs, b)
+        case ((_, lastLeftover), None) =>
+          (Nil, lastLeftover)
       }
       .flatMap {
         case (v, _) => AsyncStream.fromSeq(v)
